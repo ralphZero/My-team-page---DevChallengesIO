0e2642c10febb2ea9c0e3bd4275aa57a0b898049
ralphZero <placideralph@gmail.com>
2020-09-12 22:23:27 -0400

    finishing touches

--- effects.js
+++ effects.js
@@ -9,7 +9,7 @@ var data = [{
 },{
     'url':'photo',
     'role':'tech lead',
-    'name':'shoe le'
+    'name':'shae le'
 },{
     'url':'photo',
     'role':'ux designer',
@@ -23,3 +23,33 @@ var data = [{
     'role':'developer',
     'name':'stan john'
 }];
+console.log(data[1]['name']);
+var row = document.querySelector('.row');
+for(var i=0; i < col, c = data.length;i++){
+
+    next = i + 1;
+    
+    var col = document.createElement('div');
+    var img = document.createElement('img');
+    var content = document.createElement('div');
+    var img_box = document.createElement('div');
+    var colName = document.createElement('span');
+    var role = document.createElement('span');
+
+    col.className = 'col';
+    img_box.className = 'col-img-box';
+    content.className = 'col-content';
+    img.className = 'col-img';
+    colName.className = 'col-img-name';
+    role.className = 'col-img-role';
+
+    colName.append(document.createTextNode(data[i]['name']));
+    role.append(document.createTextNode(data[i]['role']));
+    img.src = data[i]['url']+next+'.png';
+    img_box.append(img);
+    content.append(img_box);
+    content.append(colName);
+    col.append(content);
+    col.append(role);
+    row.append(col);
+}
