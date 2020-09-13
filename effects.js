f456f51abc43051d5b5557e7c5d4ae9653b3a30a
ralphZero <placideralph@gmail.com>
2020-09-12 22:23:06 -0400

    fixing css

---
+++ effects.js
@@ -0,0 +1,25 @@
+var data = [{
+    'url':'photo',
+    'role':'product owner',
+    'name':'bill mahoney'
+},{
+    'url':'photo',
+    'role':'art director',
+    'name':'saba cabrera'
+},{
+    'url':'photo',
+    'role':'tech lead',
+    'name':'shoe le'
+},{
+    'url':'photo',
+    'role':'ux designer',
+    'name':'skylah lu'
+},{
+    'url':'photo',
+    'role':'developer',
+    'name':'griff richards'
+},{
+    'url':'photo',
+    'role':'developer',
+    'name':'stan john'
+}];
